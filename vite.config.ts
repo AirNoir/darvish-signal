01d901c14947d5f7fdf852daa38fd466260342c2
Williams <88303742+KuoYing0504@users.noreply.github.com>
2026-03-09 01:45:50 +0800

    update vite.config.js

--- vite.config.ts
+++ vite.config.ts
@@ -5,4 +5,12 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [vue(), tailwindcss()],
+  server: {
+    proxy: {
+      '/api': {
+        target: 'https://twstockapi-testnet.up.railway.app',
+        changeOrigin: true,
+      },
+    },
+  },
 })
